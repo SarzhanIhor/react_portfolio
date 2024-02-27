a6f5e7c123f378a4ea9dd610297b13e56f6c287d
SarzhanIhor <75244246+SarzhanIhor@users.noreply.github.com>
2024-02-27 19:15:15 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
     <main>
       <Hero/>
     </main>
-    
   </>
   
 }
--- src/assets/images.js
+++ src/assets/images.js
@@ -1,6 +1,71 @@
-import avatar from "./Avatar.png"
+import client from "./Client.png"
+import client2 from "./Client2.png"
+import client3 from "./Client3.png"
+import barsSolidWhite from "./bars-solid white.svg"
+import barsSolid from "./bars-solid.svg"
+import css3 from "./css3-alt.svg"
+import email from "./email.svg"
+import github from "./Github.svg"
+import heroImg from "./hero-img.png"
+import html5 from "./html5.svg"
+import isntagram from "./Instagram.svg"
+import jqueryIcon from "./jquery-icon.svg"
+import js  from "./js.svg"
+import leftArrow from "./left-arrow.svg"
+import linkSolid from "./link-solid.svg"
+import linkedin from "./Linkedin.svg"
+import loc from "./loc.svg"
+import logo from "./logo.svg"
+import moon from "./moon.svg"
+import phone from "./phone.svg"
+import portfolio1 from "./portfolio1.jpg"
+import portfolio2 from "./portfolio2.jpg"
+import portfolio3 from "./portfolio3.jpg"
+import react from "./react.svg"
+import rightArrow from "./right-arrow.svg"
+import sass from "./sass.svg"
+import sun from "./sun.svg"
+import thumbnail from "./Thumbnail.jpg"
+import thumbnail2 from "./Tthumbnail2.jpg"
+import thumbnail3 from "./Tthumbnail3.jpg"
+import twitter from "./Twitter.svg"
+import xmarkSolidWhite from "./xmark-solid white.svg"
+import xmarkSolid from "./xmark-solid.svg"
+
+
 
 export default {
-    avatar,
-    
+    client,
+    client2,
+    client3,
+    barsSolidWhite,
+    barsSolid,
+    css3,
+    email,
+    github,
+    heroImg,
+    html5,
+    isntagram,
+    jqueryIcon,
+    js,
+    leftArrow,
+    linkSolid,
+    linkedin,
+    loc,
+    logo,
+    moon,
+    phone,
+    portfolio1,
+    portfolio2,
+    portfolio3,
+    react,
+    rightArrow,
+    sass,
+    sun,
+    thumbnail,
+    thumbnail2,
+    thumbnail3,
+    twitter,
+    xmarkSolidWhite,
+    xmarkSolid,
 }
